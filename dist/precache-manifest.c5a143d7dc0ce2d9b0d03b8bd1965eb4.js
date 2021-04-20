99932ab8036735a3ab427be0b5d775203b8e901e
Dustin Woodard <dustin.woodard@gmail.com>
2021-04-19 21:38:08 -0600

    dist

---
+++ dist/precache-manifest.c5a143d7dc0ce2d9b0d03b8bd1965eb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd3c478ed8938bbe903",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8f96dabc70a35947c8ff8eeb0a353b97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dd3c478ed8938bbe903",
+    "url": "/js/app.692f5d10.js"
+  },
+  {
+    "revision": "88c4397a57fefe56b79c",
+    "url": "/js/chunk-vendors.3ccf0f64.js"
+  },
+  {
+    "revision": "beafa8a980b8cdf2674d03f3a50fe6c7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
