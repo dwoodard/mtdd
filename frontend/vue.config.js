4648421bdba9533bddb88411c3b1dcf1fa712670
Dustin Woodard <dustin.woodard@gmail.com>
2021-04-20 11:34:51 -0600

    precache

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,12 +2,23 @@ const { GenerateSW } = require("workbox-webpack-plugin");
 const withPWA = require('next-pwa')
 
 module.exports = {
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    scope: '/app',
-    sw: 'service-worker.js',
-  },
+  module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+      name: 'My App',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      
+      // configure the workbox plugin
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'dev/sw.js',
+        // ...other Workbox options...
+      }
+    }
   
   // output built static files to Laravel's public dir.
   // note the "build" script in package.json needs to be modified as well.
